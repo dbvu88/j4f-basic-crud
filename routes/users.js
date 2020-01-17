75c3e3168f4a00e136794e8ef4f8af0b8d4a7564
Duc Vu <de.yd315@gmail.com>
2020-01-16 22:24:46 -0600

    refactored codes

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,9 @@ router
   .get('/', function (req, res, next) {
     res.send('respond with a resource users');
   })
-
-router.post('/', function (req, res, next) {
-  res.send('Got a POST request')
-})
+  .post('/', function (req, res, next) {
+    res.send('Got a POST request')
+  })
   .put('/:id', (req, res, next) => {
     res.send('Got a PUT request ' + req.params.id)
   })
